43a743682cd3fc0c6a3c974724d8487197bf98ed
yse220027 <220027gs@yse-c.net>
2023-11-10 14:18:08 +0900

    update

--- tweet/like.php
+++ tweet/like.php
@@ -1,20 +1,5 @@
 <?php
-require_once '../env.php'; 
+require_once "../env.php";
 
-class LikeController extends Controller{
-    public function like(){
-        
-        $like = new Like();
-
-        if ($value = $like->has($_POST['tweet_id'], $_POST['user_id'])) {
-            $like->delete($value['id']);
-        } else {
-            $like->save($_POST);
-        }
-
-        Route::redirect('../tweet/index');
-    }
-}
-$likeController = new LikeController();
-$likeController->like();
-?>
+$controller = new TweetController();
+$controller->like();
